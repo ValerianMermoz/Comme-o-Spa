3353c4d5e79c091db86c73e2fb06ac3747e3ce94
Valérian M <valerian.mermoz@gmail.com>
2023-11-09 20:27:59 +0100

    Commit

--- comme-o-spa/src/home.jsx
+++ comme-o-spa/src/home.jsx
@@ -1,9 +1,13 @@
-
-
+import Logo from "./assets/logo.png";
+import Nav from "./components/nav";
+import Imgcarroussel from "./assets/carroussel.png";
 
 function Home() {
   return (
     <div className="homescreen">
+      <img src={Logo}></img>
+      <Nav />
+      <img src={Imgcarroussel}></img>
     </div>
   );
 }
---
+++ comme-o-spa/src/sass/03_components/nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+  return (
+    <nav className="main-nav">
+      <h2>Accueil</h2>
+      <h2>Massage à domicile</h2>
+      <h2>Contact</h2>
+    </nav>
+  );
+}
+
+export default Nav;
